ea01e3169dbcb0f8d7902a2bebed3710f083c39a
shakeelriyaj <shakeel.riyaj@gmail.com>
2023-12-23 09:31:58 -0800

    shebang line

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/Library/Frameworks/Python.framework/Versions/3.11/bin/python3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
